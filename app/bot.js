42fbbdf655d50d3b421bcb6a6f55aed5170a02ae
r.kalita <r.kalita@apomeds.com>
2024-05-26 00:39:05 +0700

    bot

--- app/bot.js
+++ app/bot.js
@@ -20,10 +20,13 @@ bot.command('start', (ctx) => {
         { json: { tg_id: userInfo?.id, tg_username: userInfo?.username } },
         function (error, response, body) {
             if (!error && response.statusCode == 200) {
-                ctx.replyWithHTML(`Hello! Click on the 'Open app' button below to launch the application \nWrite <code>/setWallet your_wallet_address</code> to set you Aptos wallet in application`,
-                Markup.inlineKeyboard([
-                    Markup.button.webApp('Open app', `${webAppUrl}/tap?tg_id=${userInfo?.id}&tg_username=${userInfo.username}`),
-                ]),);
+                // ctx.replyWithHTML(`Hello! Click on the 'Open app' button below to launch the application \nWrite <code>/setWallet your_wallet_address</code> to set you Aptos wallet in application`,
+                
+                // Markup.inlineKeyboard([
+                //     Markup.button.webApp('Open app', `${webAppUrl}/tap?tg_id=${userInfo?.id}&tg_username=${userInfo.username}`),
+                // ]),);
+
+                ctx.bot.sendPhoto(userInfo.id, 'https://aptosfomo-c4ea4.web.app/img/donut.png');
             } else {
                 ctx.reply(`Something went wrong`);
             }
