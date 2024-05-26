1aba3344912bc0821577c88fa10894df8cda0e3d
r.kalita <r.kalita@apomeds.com>
2024-05-26 18:59:20 +0700

    setwallet

--- app/bot.js
+++ app/bot.js
@@ -28,7 +28,7 @@ bot.command('start', (ctx) => {
     ctx.replyWithPhoto('https://aptosfomo-c4ea4.web.app/img/FOMSFIELD.png', { caption: `Welcome to Fomsfield, where even cats are crazy for donuts! Before we proceed, please solve this \nCAPTCHA:\n\n${question}` });
 });
 
-bot.command('setWallet', (ctx) => {
+bot.command('setwallet', (ctx) => {
     if (ctx.args.length) {
         if (AccountAddress.isValid({input: ctx.args[0]}).valid) {
 
@@ -47,7 +47,7 @@ bot.command('setWallet', (ctx) => {
             ctx.reply(`Invalid wallet address`)
         }
     } else {
-        ctx.reply(`Type: /setWallet your_wallet_address`);
+        ctx.reply(`Type: /setwallet your_wallet_address`);
     }
 });
 
@@ -76,7 +76,7 @@ bot.on('text', (ctx) => {
 
                 if (!error && response.statusCode == 200) {
                     ctx.reply(`That's right!\n Click on the 'Open app' button below to launch the application`).then(() => {
-                        return ctx.replyWithHTML(`Write <code>/setWallet your_wallet_address</code> \uD83D\uDCCB to set you Aptos wallet in application`,
+                        return ctx.replyWithHTML(`Write <code>/setwallet your_wallet_address</code> \uD83D\uDCCB to set you Aptos wallet in application`,
                         Markup.inlineKeyboard([
                             Markup.button.webApp('Open app', `${webAppUrl}/tap?tg_id=${userInfo?.id}&tg_username=${userInfo.username}`),
                         ]),);
