eacb43f637f75e023a50859a4e044e05df0a1e33
r.kalita <r.kalita@apomeds.com>
2024-06-06 19:11:28 +0700

    fix

--- app/bot.js
+++ app/bot.js
@@ -62,17 +62,17 @@ bot.on(message('web_app_data'), async (ctx) => {
 });
 
 bot.command('buy', (ctx) => {
-    ctx.replyWithHTML(`⚡️How To Buy GOLD DONUTS with $FOMO⚡️\n
-    1) Send min. 1.000.000 (1m) $FOMO to fomo-donut.apt\n
-    2) Launch game. Go to "Explore" and click on "Claim Donuts"\n
-    3) Done\n\n
+    ctx.replyWithHTML(`⚡️How To Buy GOLD DONUTS with $FOMO⚡️
+    1) Send min. 1.000.000 (1m) $FOMO to fomo-donut.apt
+    2) Launch game. Go to "Explore" and click on "Claim Donuts"
+    3) Done
     
-    ⚠️IMPORTANT⚠️\n
-    Minimum amount to send is 1million $FOMO (6 gold donuts) . You can send any amount with round numbers. For example 1..2...3...4...5... millions fomo. In case if you sent not round amount (ex. 999.999 , 1.500.000...) = funds will be refunded.\n\n
+    ⚠️IMPORTANT⚠️
+    Minimum amount to send is 1million $FOMO (6 gold donuts) . You can send any amount with round numbers. For example 1..2...3...4...5... millions fomo. In case if you sent not round amount (ex. 999.999 , 1.500.000...) = funds will be refunded.
     
-    🪙How To Buy $FOMO:🪙\n
-    1) Go to https://baptswap.com/#/swap\n
-    2) Select APT/ APTOS FOMO pair \n
+    🪙How To Buy $FOMO:🪙
+    1) Go to https://baptswap.com/#/swap
+    2) Select APT/ APTOS FOMO pair 
     3) Buy amount you want`);
 });
 
