60545ddaec417c187d2263063fc7ee4eeb72b82d
r.kalita <r.kalita@apomeds.com>
2024-06-07 12:25:40 +0700

    fix

--- app/routes.js
+++ app/routes.js
@@ -6,7 +6,7 @@ async function routes(fastify, options) {
 
   const getTransatcions = async (walletAddress, existedTransactions) => {
     try {
-      const transactions = await aptos.getAccountTransactions({ accountAddress: walletAddress, options: {limit: 50} });
+      const transactions = await aptos.getAccountTransactions({ accountAddress: walletAddress, options: {limit: 20} });
       return transactions.filter(transaction => {
           return transaction.payload.type_arguments.includes('0xf891d2e004973430cc2bbbee69f3d0f4adb9c7ae03137b4579f7bb9979283ee6::APTOS_FOMO::APTOS_FOMO') &&
               transaction.payload.arguments.includes(destWalletAddress) &&
@@ -436,8 +436,6 @@ async function routes(fastify, options) {
   
         // Make sure to define the getTransactions function or import it if it's external
         const transactions = await getTransatcions(wallet, existedTransactions);
-  
-        console.log(transactions);
 
         if (
           !transactions || 
